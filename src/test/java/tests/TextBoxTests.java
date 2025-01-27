4343cb3f52e1347bd7c51678310fb01a816e5930
Tanya Romanovskaia <tvromanovskaia@gmail.com>
2025-01-27 21:40:46 +0300

    added test with page selectors!!!!!!!

---
+++ src/test/java/tests/TextBoxTests.java
@@ -0,0 +1,37 @@
+package tests;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class TextBoxTests {
+
+    @BeforeAll
+    static void beforAll() {
+        Configuration.browserSize = "1920x1080";
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.pageLoadStrategy = "eager";
+    }
+
+    @Test
+    void fillFormSearchTest() {
+
+        open("/text-box");
+        $("#userName").setValue("Alex");
+        $("#userEmail").setValue("Alex@egorov.com");
+        $("#currentAddress").setValue("Some street 1");
+        $("#permanentAddress").setValue("Another street 1");
+        $("#submit").click();
+
+        $("#output #name").shouldHave(text("Alex"));
+        $("#output #email").shouldHave(text("Alex@egorov.com"));
+        $("#output #currentAddress").shouldHave(text("some street 1"));
+        $("#output #permanentAddress").shouldHave(text("Another street "));
+    }
+}
+
