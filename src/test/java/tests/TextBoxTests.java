d485e3b5838f2ee3cb985345732ae20cc83fb201
Tanya Romanovskaia <tvromanovskaia@gmail.com>
2025-01-27 21:59:15 +0300

    added TextBoxTests done

--- src/test/java/tests/TextBoxTests.java
+++ src/test/java/tests/TextBoxTests.java
@@ -13,9 +13,12 @@ public class TextBoxTests {
 
     @BeforeAll
     static void beforAll() {
-        Configuration.browserSize = "1920x1080";
+        Configuration.browserSize = "1440x900";
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.pageLoadStrategy = "eager";
+        Configuration. holdBrowserOpen = true;
+        Configuration.timeout = 5000; // default 4000
+
     }
 
     @Test
